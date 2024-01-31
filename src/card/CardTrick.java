03630f1c7d6ee5170acede7ad9b08cce9a2f53df
kau15445 <158235572+kau15445@users.noreply.github.com>
2024-01-31 17:05:50 -0500

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 /*
- * @modifier : Kirandeep Kaur;
+ * @modifier : Kirandeep Kaur
  * Student Number: 991715924
  */
 public class CardTrick {
@@ -13,39 +13,50 @@ public class CardTrick {
         Card[] magicHand = new Card[7];
         Random random = new Random();
 
-        
+       
         for (int i = 0; i < magicHand.length; i++) {
-            int number = random.nextInt(13) + 1; 
-            String suit = Card.SUITS[random.nextInt(Card.SUITS.length)]; 
+            int number = random.nextInt(13) + 1;
+            String suit = Card.SUITS[random.nextInt(Card.SUITS.length)];
             magicHand[i] = new Card(number, suit);
         }
 
-      
+       
+        Card luckyCard = new Card(7, "Hearts"); 
+
+     
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a card number (1-13):");
         int value = scanner.nextInt();
         System.out.println("Enter a suit (Hearts, Diamonds, Clubs, Spades):");
-        scanner.nextLine();  
+        scanner.nextLine();
         String suit = scanner.nextLine();
 
-      
-        boolean found = false;
+       
+        boolean found = false, luckyFound = false;
         for (Card card : magicHand) {
             if (card.getNumber() == value && card.getSuit().equalsIgnoreCase(suit)) {
                 found = true;
-                break;
+            }
+            if (card.getNumber() == luckyCard.getNumber() && card.getSuit().equals(luckyCard.getSuit())) {
+                luckyFound = true;
             }
         }
 
-      
+       
         if (found) {
             System.out.println("Your card was found in the magic hand!");
         } else {
             System.out.println("Your card was not found in the magic hand.");
         }
+
+       
+        if (luckyFound) {
+            System.out.println("The lucky card was found in the magic hand!");
+        } else {
+            System.out.println("The lucky card was not found in the magic hand.");
+        }
     }
 
-    
     static class Card {
         private final int number;
         private final String suit;
@@ -66,3 +77,6 @@ public class CardTrick {
         }
     }
 }
+
+       
+       
